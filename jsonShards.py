14e4135770ebc62f7673fb9da950bf53871b6b97
Tomaz Bracic <tomaz.bracic@gmail.com>
2015-08-25 16:13:04 +0200

    working sequence changing from and into JSON file as storage

--- jsonShards.py
+++ jsonShards.py
@@ -7,9 +7,27 @@ jsonFile.close()
 
 print("shards read ", shards)
 
+print(len(shards))
+
+new_shards = []
 
 for shard in shards:
-    print(shard['shard_id'])
+    #for x in range(1, len(shards)):
+    print(shard['shard_id'], " in seq je : ", shard['seq'])
+    print("star seq", shard['seq'])
+    new_seq = int(shard['seq']) + int(shard['seq'])
+    print("nov seq", new_seq)
+
+    new_shard = {}
+
+    new_shard['shard_id'] = shard['shard_id']
+    new_shard['seq'] = new_seq
+    new_shards.append(new_shard)
+
+
+print(new_shards)
+
+
     #print(shards[shard]['shard_id'])
 #shards = [
 #      {'shard_id': 'shard-0000', 'seq': '123'},
@@ -20,5 +38,5 @@ for shard in shards:
 
 
 jsonFile = open("shards.json", "w+")
-jsonFile.write(json.dumps(shards))
+jsonFile.write(json.dumps(new_shards))
 jsonFile.close()
